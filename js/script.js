9e0070822be701095e763f40d8ef9dcf7791dd4e
ZaHoussam <mrlhou62@gmail.com>
2021-09-18 18:38:05 +0200

    creat web color

---
+++ js/script.js
@@ -0,0 +1,334 @@
+$(document).ready(function () {
+    $(".color1").click(function () { 
+       color1();
+       $(".page1>p>span").addClass("style_Paragraph");
+       $(".page1>p").addClass("font_family_word");
+       removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+       removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+       removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+       removeColor9();
+
+    });
+
+    $(".color2").click(function () { 
+        color2();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor1();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color3").click(function () { 
+        color3();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor1();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color4").click(function () { 
+        color4();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor1();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color5").click(function () { 
+         color5();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor1();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color6").click(function () { 
+        color6();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor1();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color7").click(function () { 
+        color7();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor1();removeColor8();
+        removeColor9();
+     });
+     $(".color8").click(function () { 
+        color8();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor1();
+        removeColor9();
+     });
+     $(".color9").click(function () { 
+        color9();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor1();
+     });
+     $(".color10").click(function () { 
+        color10();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor1();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color11").click(function () { 
+        color11();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor1();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color12").click(function () { 
+        color12();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor1();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color13").click(function () { 
+        color13();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor1();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color14").click(function () { 
+        color14();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor1();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color15").click(function () { 
+        color15();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor1();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+
+     $(".color16").click(function () { 
+        color16();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor1();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color17").click(function () { 
+        color17();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor1();removeColor18();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color18").click(function () { 
+        color18();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor1();removeColor19();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color19").click(function () { 
+        color19();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor1();removeColor20();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+
+     $(".color20").click(function () { 
+        color20();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor1();
+        removeColor21();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+     $(".color21").click(function () { 
+        color21();
+        $(".page1>p>span").addClass("style_Paragraph");
+        $(".page1>p").addClass("font_family_word");
+        removeColor2();removeColor10();removeColor11();removeColor12();removeColor13();removeColor14();
+        removeColor15();removeColor16();removeColor17();removeColor18();removeColor19();removeColor20();
+        removeColor1();removeColor3();removeColor4();removeColor5();removeColor6();removeColor7();removeColor8();
+        removeColor9();
+     });
+
+     /* FUNCTION ADD */
+
+    function color1() {
+        $("#part1").addClass("color1").fadeIn("fast");
+    }
+    function color2() {
+        $("#part1").addClass("color2");
+    }
+    function color3() {
+        $("#part1").addClass("color3");
+    }
+    function color4() {
+        $("#part1").addClass("color4");
+    }
+    function color5() {
+        $("#part1").addClass("color5");
+    }
+    function color6() {
+        $("#part1").addClass("color6");
+    }
+    function color7() {
+        $("#part1").addClass("color7");
+    }
+    function color8() {
+        $("#part1").addClass("color8");
+    }
+    function color9() {
+        $("#part1").addClass("color9");
+    }
+    function color10() {
+        $("#part1").addClass("color10");
+    }
+    function color11() {
+        $("#part1").addClass("color11");
+    }
+    function color12() {
+        $("#part1").addClass("color12");
+    }
+    function color13() {
+        $("#part1").addClass("color13");
+    }
+    function color14() {
+        $("#part1").addClass("color14");
+    }
+    function color15() {
+        $("#part1").addClass("color15");
+    }
+    function color16() {
+        $("#part1").addClass("color16");
+    }
+    function color17() {
+        $("#part1").addClass("color17");
+    }
+    function color18() {
+        $("#part1").addClass("color18");
+    }
+    function color19() {
+        $("#part1").addClass("color19");
+    }
+    function color20() {
+        $("#part1").addClass("color20");
+    }
+    function color21() {
+        $("#part1").addClass("color21");
+    }
+
+    /* FUNCTION REMOVE */   
+
+
+    function removeColor1() {
+        $("#part1").removeClass("color1");
+    }
+    function removeColor2() {
+        $("#part1").removeClass("color2");
+    }
+    function removeColor3() {
+        $("#part1").removeClass("color3");
+    }
+    function removeColor3() {
+        $("#part1").removeClass("color3");
+    }
+    function removeColor4() {
+        $("#part1").removeClass("color4");
+    }
+    function removeColor5() {
+        $("#part1").removeClass("color5");
+    }
+    function removeColor6() {
+        $("#part1").removeClass("color6");
+    }
+    function removeColor7() {
+        $("#part1").removeClass("color7");
+    }
+    function removeColor8() {
+        $("#part1").removeClass("color8");
+    }
+    function removeColor9() {
+        $("#part1").removeClass("color9");
+    }
+    function removeColor10() {
+        $("#part1").removeClass("color10");
+    }
+
+    function removeColor11() {
+        $("#part1").removeClass("color11");
+    }
+    function removeColor12() {
+        $("#part1").removeClass("color12");
+    }
+    function removeColor13() {
+        $("#part1").removeClass("color13");
+    }
+    function removeColor14() {
+        $("#part1").removeClass("color14");
+    }
+    function removeColor15() {
+        $("#part1").removeClass("color15");
+    }
+    function removeColor16() {
+        $("#part1").removeClass("color16");
+    }
+    function removeColor17() {
+        $("#part1").removeClass("color17");
+    }
+    function removeColor18() {
+        $("#part1").removeClass("color18");
+    }
+    function removeColor19() {
+        $("#part1").removeClass("color19");
+    }
+    function removeColor20() {
+        $("#part1").removeClass("color20");
+    }
+    function removeColor21() {
+        $("#part1").removeClass("color21");
+    }
+
+
+});
